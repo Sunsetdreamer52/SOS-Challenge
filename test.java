3e8b8d197055e459d237fc49ed04c1f1d6883a8b
Caleb <powellcalebm@gmail.com>
2020-07-01 10:53:47 -0400

    test

---
+++ test.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main
+{
+    
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Hello World");
+		System.out.println("Whats your name?");
+		String name = scan.nextline();
+	}
+}
+
