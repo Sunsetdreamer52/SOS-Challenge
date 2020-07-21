2b4f269fefe4e267c3e29c51fff3bbc9297adc0a
Caleb <powellcalebm@gmail.com>
2020-07-21 04:12:18 -0400

    adding files

--- target/classes/templates/main.js
+++ target/classes/templates/main.js
@@ -1,6 +1,16 @@
-var ourRequest= new XMLHttpRequest();
-ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
-ourRequest.onload = function(){
-    console.log(ourRequest.responseText)
+var animalContainer = document.getElementById("animal-info");
+var btn = document.getElementById("btn");
+
+btn.addEventListener("click", function() {
+    var ourRequest= new XMLHttpRequest();
+    ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
+    ourRequest.onload = function(){
+        var ourData = JSON.parse(ourRequest.responseText);
+        renderHTML(ourData);
+    };
+    ourRequest.send();
+});
+
+function renderHTML(data) {
+    animalContainer.insertAdjacentHTML('beforeend', 'testing 123');
 };
-ourRequest.send;
